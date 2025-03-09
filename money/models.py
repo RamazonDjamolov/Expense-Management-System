4ecf0e0884aa1b39cf68f83c8340d7ae3ffb301b
djamolovramazon <djamolovramazon90@gmail.com>
2025-03-09 18:14:11 +0500

    19 normativ

---
+++ money/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'category'
+
+    def __str__(self):
+        return self.name
+
+
+class Income(models.Model):
+    amount = models.DecimalField(decimal_places=2, max_digits=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'income'
+
+
+class Expense(models.Model):
+    amount = models.DecimalField(decimal_places=2, max_digits=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
