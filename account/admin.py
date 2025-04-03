b9ab723c22a33d832d044bfbe6616261e120d1ad
djamolovramazon <djamolovramazon90@gmail.com>
2025-04-03 22:59:41 +0500

    normativga tayorlov

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+
+from account.models import User
+
 
 # Register your models here.
+@admin.register(User)
+class Admin(admin.ModelAdmin):
+    pass
+
+@admin.register(Permission)
+class Admin(admin.ModelAdmin):
+    pass
