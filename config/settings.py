f69d81f7583d420310d0d0d9ab7e1a23f199c3a0
djamolovramazon <djamolovramazon90@gmail.com>
2025-04-07 16:17:36 +0500

    23 tugadi

--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'money.middleware.AutoLogoutMiddleware',
 
     # 'money.middleware.LoggIPWriterMiddleware',
     # 'money.middleware.BlockIpMiddleware',
--- money/middleware.py
+++ money/middleware.py
@@ -1,2 +1,28 @@
-from django.http import HttpResponseForbidden, HttpResponse
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 from django.utils import timezone
+
+from django.utils.deprecation import MiddlewareMixin
+
+
+class AutoLogoutMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = getattr(request, 'user', None)
+
+        if user and user.is_authenticated:
+            last_login = request.user.last_login
+            if last_login:
+                current_time = timezone.now()
+                seconds_pas = (current_time - last_login).total_seconds()
+                if seconds_pas > 10:
+                    logout(request)
+                    request.session.flush()
+
+                    return redirect('account:login')
+
+        response = self.get_response(request)
+        return response
+
